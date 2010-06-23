f68e13c0fe05510a15bba4c08170797b8c15b75d
Pekka Enberg <penberg@reaktor.fi>
2010-06-23 13:50:05 +0300

    Extract StubServer from ConnectionTest#garbledMessage
    This patch extracts StubServer class from test code so that we can reuse it in
    new test cases.
    
    Signed-off-by: Pekka Enberg <penberg@reaktor.fi>

--- src/test/java/silvertip/ConnectionTest.java
+++ src/test/java/silvertip/ConnectionTest.java
@@ -17,35 +17,10 @@ public class ConnectionTest {
   public void garbledMessage() throws Exception {
     final int port = 4444;
     final String message = "The quick brown fox jumps over the lazy dog";
-    final CountDownLatch serverStarted = new CountDownLatch(1);
-    final CountDownLatch serverStopped = new CountDownLatch(1);
-    final CountDownLatch clientStopped = new CountDownLatch(1);
-    Thread server = new Thread(new Runnable() {
-      @Override
-      public void run() {
-        ServerSocket serverSocket = null;
-        try {
-          serverSocket = new ServerSocket(port);
-          serverStarted.countDown();
-          Socket clientSocket = serverSocket.accept();
-          clientSocket.getOutputStream().write(message.getBytes());
-          clientSocket.getOutputStream().flush();
-          clientStopped.await();
-        } catch (Exception e) {
-          throw new RuntimeException(e);
-        } finally {
-          if (serverSocket != null) {
-            try {
-              serverSocket.close();
-            } catch (IOException ex) {
-            }
-          }
-          serverStopped.countDown();
-        }
-      }
-    });
-    server.start();
-    serverStarted.await();
+    StubServer server = new StubServer(port, message);
+    Thread serverThread = new Thread(server);
+    serverThread.start();
+    server.awaitForStart();
     try {
       final Connection connection = Connection.connect(new InetSocketAddress("localhost", port), 1000,
           new MessageParser() {
@@ -69,8 +44,56 @@ public class ConnectionTest {
         }
       });
     } finally {
+      server.notifyClientStopped();
+      server.awaitForStop();
+    }
+  }
+
+  private final class StubServer implements Runnable {
+    private final CountDownLatch serverStopped = new CountDownLatch(1);
+    private final CountDownLatch serverStarted = new CountDownLatch(1);
+    private final CountDownLatch clientStopped = new CountDownLatch(1);
+    private final String message;
+    private final int port;
+
+    private StubServer(int port, String message) {
+      this.message = message;
+      this.port = port;
+    }
+
+    public void awaitForStart() throws InterruptedException {
+      serverStarted.await();
+    }
+
+    public void notifyClientStopped() {
       clientStopped.countDown();
+    }
+
+    public void awaitForStop() throws InterruptedException {
       serverStopped.await();
+    }
+
+    @Override
+    public void run() {
+      ServerSocket serverSocket = null;
+      try {
+        serverSocket = new ServerSocket(port);
+        serverStarted.countDown();
+        Socket clientSocket = serverSocket.accept();
+        clientSocket.getOutputStream().write(message.getBytes());
+        clientSocket.getOutputStream().flush();
+        clientStopped.await();
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      } finally {
+        if (serverSocket != null) {
+          try {
+            serverSocket.close();
+          } catch (IOException ex) {
+          }
+        }
+        serverStopped.countDown();
+      }
     }
   }
 }
