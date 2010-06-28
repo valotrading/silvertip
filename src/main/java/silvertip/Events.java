e958525e4ddb8881579c43682faa927fb04165ae
Pekka Enberg <penberg@reaktor.fi>
2010-06-28 16:26:13 +0300

    silvertip: Update Events class Javadoc
    Signed-off-by: Pekka Enberg <penberg@reaktor.fi>

--- src/main/java/silvertip/Events.java
+++ src/main/java/silvertip/Events.java
@@ -8,6 +8,39 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * The <code>Events</code> class is the heart of Silvertip, an event
+ * notification API for Java. The class is a wrapper on top of NIO
+ * <code>Selector</code> and can be used for polling one or more event sources
+ * (TCP sockets, for example) as efficiently as possible. On Linux, for example,
+ * the JVM uses <code>epoll_ctl(2)</code> and <code>epoll_wait(2)</code>, under
+ * the hood for <code>Events#register</code> and <code>Events#dispatch</code>,
+ * respectively.
+ * <p>
+ * To use the API, you need to instantiate a new <code>Events</code> object and
+ * register one or more <code>EventSource</code>s to it. You can then invoke
+ * <code>Events#dispatch</code> to enter event dispatch loop that returns only
+ * if all event sources unregister themselves or you invoke
+ * <code>EventSource#stop</code>.
+ * <p>
+ * A simple example looks like this:
+ *
+ * <pre>
+ *   Events events = Events.open(30*1000); // 30 second timeout
+ *   Connection connection = Connection.connect(..., new Connection.Callback() {
+ *     public void messages(Connection connection, Iterator<Message> messages) {
+ *       while (messages.hasNext()) {
+ *         System.out.println(messages.next());
+ *       }
+ *     }
+ *     public void idle(Connection connection) {
+ *       // This callback is called every 30 seconds if there's no activity.
+ *     }
+ *   }));
+ *   events.register(connection);
+ *   events.dispatch();
+ * </pre>
+ */
 public class Events {
   private List<EventSource> sources = new ArrayList<EventSource>();
   private Selector selector;
