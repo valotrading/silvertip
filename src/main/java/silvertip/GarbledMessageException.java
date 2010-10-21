ad5891f0ed383c1c67d15fb839bb5705ad7759eb
Karim Osman <karim.osman@reaktor.fi>
2010-10-21 17:49:16 +0300

    silvertip: Include data of the garbled message in GarbledMessageException
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- src/main/java/silvertip/GarbledMessageException.java
+++ src/main/java/silvertip/GarbledMessageException.java
@@ -2,4 +2,14 @@ package silvertip;
 
 public class GarbledMessageException extends Exception {
   private static final long serialVersionUID = 1L;
+
+  private byte[] messageData;
+
+  public byte[] getMessageData() {
+    return messageData;
+  }
+
+  public void setMessageData(byte[] messageData) {
+    this.messageData = messageData;
+  }
 }
