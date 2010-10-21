9072985e53de2488276cfbdda41406143185a523
Karim Osman <karim.osman@reaktor.fi>
2010-10-21 17:49:12 +0300

    silvertip: Add enumeration for tags in FixMessageParser
    Tag enumeration comprises of tag number and name. This allows more
    detailed error messages when a message is considered garbled.
    
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Kari Husa <kari.husa@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- src/main/java/silvertip/protocols/FixMessageParser.java
+++ src/main/java/silvertip/protocols/FixMessageParser.java
@@ -31,8 +31,8 @@ public class FixMessageParser extends AbstractMessageParser<Message> {
 
   private FixMessageHeader header(ByteBuffer buffer) throws GarbledMessageException {
     int start = buffer.position();
-    parseField(buffer, 8);
-    String bodyLength = parseField(buffer, 9);
+    parseField(buffer, Tag.BEGIN_STRING);
+    String bodyLength = parseField(buffer, Tag.BODY_LENGTH);
 
     return new FixMessageHeader(buffer.position() - start, Integer.parseInt(bodyLength));
   }
@@ -40,7 +40,7 @@ public class FixMessageParser extends AbstractMessageParser<Message> {
   private int trailer(ByteBuffer buffer) throws GarbledMessageException {
     int start = buffer.position();
     try {
-      parseField(buffer, 10);
+      parseField(buffer, Tag.CHECKSUM);
     } catch (GarbledMessageException e) {
       buffer.position(start);
       throw e;
@@ -48,15 +48,16 @@ public class FixMessageParser extends AbstractMessageParser<Message> {
     return buffer.position() - start;
   }
 
-  private String parseField(ByteBuffer buffer, int tag) throws GarbledMessageException {
-    match(buffer, tag + "=");
+  private String parseField(ByteBuffer buffer, Tag tag) throws GarbledMessageException {
+    match(buffer, tag);
     return value(buffer);
   }
 
-  private void match(ByteBuffer buffer, String s) throws GarbledMessageException {
-    for (int i = 0; i < s.length(); i++) {
+  private void match(ByteBuffer buffer, Tag tag) throws GarbledMessageException {
+    String expected = tag.number() + "=";
+    for (int i = 0; i < expected.length(); i++) {
       int c = buffer.get();
-      if (c != s.charAt(i)) {
+      if (c != expected.charAt(i)) {
         throw new GarbledMessageException();
       }
     }
@@ -88,6 +89,29 @@ public class FixMessageParser extends AbstractMessageParser<Message> {
 
     public int getBodyLength() {
       return bodyLength;
+    }
+  }
+
+  private enum Tag {
+    BEGIN_STRING(8, "BeginString"),
+    BODY_LENGTH(9, "BodyLength"),
+    MSG_TYPE(35, "MsgType"),
+    CHECKSUM(10, "CheckSum");
+
+    private int tagNumber;
+    private String name;
+
+    private Tag(int tagNumber, String name) {
+      this.tagNumber = tagNumber;
+      this.name = name;
+    }
+
+    public int number() {
+      return tagNumber;
+    }
+
+    public String toString() {
+      return String.format("%s(%d)", name, tagNumber);
     }
   }
 }
